8286fcca4daac0fa22380e70a19c5694366d85e3
Natalia Lankevich <nataliiks@rambler.ru>
2022-02-24 23:15:54 +0300

    feat: add solution for unique brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) { 
+
+  const openBrackets = ['(', '[', '{', '1', '3', '5'];
+  const repeatBrackets = ['|' , '7', '8'];
+  const bracketsPairs ={
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',   
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',   
+  };
+ 
+
+  let stack = [];
+   for (let i = 0; i < str.length; i++) {
+     let currentSymbol = str[i];
+
+     
+    
+     if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+     } else {
+       if(stack.length === 0) {
+         return false
+       }
+
+       let lastElement = stack[stack.length - 1];
+
+
+       if (bracketsPairs[currentSymbol] ===  lastElement) {
+         stack.pop();
+       } else{
+         return false;
+       }
+     }
+  
+   
+  }
+  
+  return stack.length === 0;
 }
