792d0a76a2422b5fe9b64c91fd890199dca5dbcc
Natalia Lankevich <nataliiks@rambler.ru>
2022-02-25 20:31:01 +0300

    feat: add solution for identical brackets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) { 
 
   const openBrackets = ['(', '[', '{', '1', '3', '5'];
-  const repeatBrackets = ['|' , '7', '8'];
+  const identicalBrackets = ['|' , '7', '8'];
   const bracketsPairs ={
     [')']: '(',
     [']']: '[',
@@ -10,33 +10,47 @@ module.exports = function check(str, bracketsConfig) {
     ['4']: '3',
     ['6']: '5',   
   };
- 
+  
+  let bracket = {}; 
 
   let stack = [];
-   for (let i = 0; i < str.length; i++) {
-     let currentSymbol = str[i];
 
-     
+  for (let i = 0; i < str.length; i++) {
     
-     if (openBrackets.includes(currentSymbol)) {
+    let currentSymbol = str[i];
+    let lastEl= stack[stack.length - 1];
+
+    if (identicalBrackets.includes(currentSymbol) && !bracket[currentSymbol]) {
+      bracket[currentSymbol] = currentSymbol;
+      stack.push(currentSymbol);    
+
+    } else if (identicalBrackets.includes(currentSymbol)  && currentSymbol  ===  lastEl){
+      stack.pop();
+      delete bracket[currentSymbol];        
+    } 
+  
+    if (openBrackets.includes(currentSymbol)) {
       stack.push(currentSymbol);
      } else {
-       if(stack.length === 0) {
+       if(!identicalBrackets.includes(currentSymbol) && stack.length === 0) {
          return false
        }
 
        let lastElement = stack[stack.length - 1];
 
 
-       if (bracketsPairs[currentSymbol] ===  lastElement) {
+       if (!identicalBrackets.includes(currentSymbol) && bracketsPairs[currentSymbol] ===  lastElement) {
          stack.pop();
-       } else{
+       } else if (!identicalBrackets.includes(currentSymbol) && bracketsPairs[currentSymbol] !=  lastElement) {
          return false;
        }
-     }
+     
+    }
   
    
-  }
   
+    }
   return stack.length === 0;
+
+ 
 }
